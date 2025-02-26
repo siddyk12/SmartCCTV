e8172f22d270c7c66dd045ee375a56f596f28912
siddyk12 <siddkalra12@gmail.com>
2025-02-26 13:08:52 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,2 +1,8 @@
 # Placeholder for training script
-print("Training script will be added soon.")
+# This script will train the deep learning model for CCTV anomaly detection
+
+def train():
+    print("Training script will be added soon.")
+
+if __name__ == "__main__":
+    train()
